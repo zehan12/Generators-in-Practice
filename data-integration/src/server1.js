2278f6c61a2b46fade8ad6f3b606bab018ddf127
Zehan Khan <zkhan@hootboard.com>
2023-09-22 23:40:35 +0530

    API is created for both the servers

--- data-integration/src/server1.js
+++ data-integration/src/server1.js
@@ -1,10 +1,18 @@
-import { createServer } from "http";
+import { createServer } from "node:http";
+import { parse } from "node:url";
+import { randomUUID } from "node:crypto";
+
 const PORT = 3001;
 
 async function handler(req, res) {
-    if ( req.method === "GET" && req.url.includes("product") ) {
-        console.log("request:",req.url)
-    }
+  if (req.method === "GET" && req.url.includes("product")) {
+    const { query: { name } } = parse(req.url,true);
+    const item = {
+        product:name,
+        id: randomUUID()
+    };
+    return res.end(JSON.stringify(item));
+  }
 }
 
 createServer(handler)
--- data-integration/src/server2.js
+++ data-integration/src/server2.js
@@ -1,8 +1,16 @@
-import { createServer } from "http";
+// curl -X POST "localhost:4001/cart" --data '{"id":"123"}'
+import { createServer } from "node:http";
+import { once } from "node:events";
+
 const PORT = 4001;
 
 async function handler(req, res) {
-    res.end("hey");
+  if (req.method === "POST" && req.url.includes("cart")) {
+    const data = await once(req, "data");
+    const item = JSON.parse(data);
+    console.log("received", item);
+    return res.end(`process succeeded for ${item.id}`);
+  }
 }
 
 createServer(handler)
