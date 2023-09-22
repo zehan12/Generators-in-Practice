4fa85049fc15b8bc45f946bebb9cdd723ffa4f39
Zehan Khan <zkhan@hootboard.com>
2023-09-23 00:03:25 +0530

    resolve problem by genrator function

--- data-integration/src/client.js
+++ data-integration/src/client.js
@@ -16,21 +16,44 @@ const DB = async () =>
 const PRODUCTS_API = "http://localhost:3001/products";
 const CART_API = "http://localhost:4001/cart";
 
-const processData = async () => {
-  const products = await DB();
-  const responses = [];
-  for (const product of products) {
-    const productInfo = await (
-      await fetch(`${PRODUCTS_API}?name=${product}`)
-    ).text();
-    responses.push(productInfo);
-    const cartInfo = await (await fetch(CART_API, {
-      method: "POST",
-      body: productInfo,
-    })).text();
-    responses.push(cartInfo);
-  }
-  return responses;
-};
+// const processData = async () => {
+//   const products = await DB();
+//   const responses = [];
+//   for (const product of products) {
+//     const productInfo = await (
+//       await fetch(`${PRODUCTS_API}?name=${product}`)
+//     ).text();
+//     responses.push(productInfo);
+//     const cartInfo = await (await fetch(CART_API, {
+//       method: "POST",
+//       body: productInfo,
+//     })).text();
+//     responses.push(cartInfo);
+//   }
+//   return responses;
+// };
 
-console.table(await processData());
+// console.table(await processData());
+
+async function* processDBDataGen() {
+    const products = await DB();
+    const responses = [];
+    for (const product of products) {
+      const productInfo = await (
+        await fetch(`${PRODUCTS_API}?name=${product}`)
+      ).text();
+      responses.push(productInfo);
+      const cartInfo = await (
+        await fetch(CART_API, {
+          method: "POST",
+          body: productInfo,
+        })
+      ).text();
+      yield cartInfo;
+    }
+  }
+
+  for await ( const data of processDBDataGen() ) {
+    console.table(data);
+  }
+
